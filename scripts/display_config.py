62da75f91c6ba4a2f574a147641935173a5f05be
Paul Artushkov <pavTiger@gmail.com>
2023-05-24 17:07:41 +0300

    Add script that monitors changes in xrandr and configures displays

---
+++ scripts/display_config.py
@@ -0,0 +1,5 @@
+states = {
+    repr(["eDP-1", "DP-2-2"]): ["xrandr --output DP-2-2 --mode 1920x1080 --primary --auto", "xrandr --output eDP-1 --mode 1920x1080 --left-of DP-2-2"],
+    repr(["eDP-1"]): ["xrandr --output eDP-1 --mode 1920x1080 --primary"]
+}
+
